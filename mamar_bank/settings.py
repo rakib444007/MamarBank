4264d2400551bad114a1761bfc4f73f68a236a58
rakib444007 <rh444007@gmail.com>
2024-11-04 19:50:27 +0600

    changes of static

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -131,7 +131,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 # STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS =[
-    BASE_DIR/'static',
+    BASE_DIR/'static/img',
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
