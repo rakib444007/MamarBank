f2ea779f7e456d758b718c99c3502ddfb5dbf768
rakib444007 <rh444007@gmail.com>
2024-11-04 20:22:15 +0600

    env changes

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -27,8 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = env("DEBUG")
 ALLOWED_HOSTS = ["*"]
 
 
