7e66dbc565279e980e2cb840771fae326d01bf98
rakib444007 <rh444007@gmail.com>
2024-11-04 20:09:22 +0600

    again static changes

--- mamar_bank/settings.py
+++ mamar_bank/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import dj_database_url
 import environ
+import os
 env = environ.Env()
 environ.Env.read_env()
 from pathlib import Path
@@ -128,11 +129,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
-# STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS =[
-    BASE_DIR/'static',
-]
+# STATIC_URL = 'static/'
+# # STATIC_ROOT = BASE_DIR / 'static'
+# STATICFILES_DIRS =[
+#     BASE_DIR/'static',
+# ]
+
+STATIC_URL = '/static/'
+
+# Add these settings for deployment
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Path where static files will be collected
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')] 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
